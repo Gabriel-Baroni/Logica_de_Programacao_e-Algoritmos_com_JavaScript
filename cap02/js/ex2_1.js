5de3a8c91f44a1a6e3ee29b1795e6e4d0f6de677
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2025-01-20 15:42:31 -0300

    exemplo 2.1

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,11 @@
+//cria referencia ao from e ao elemento h3 
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//crie um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inNome.value //obtém o nome digitado no from
+    resp.innerText = `Olá`//exibe a resposta do programa
+    e.preventDefault() //evita envio do form 
+})
+
